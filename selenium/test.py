cf5816dfb423d9b4a37777d6a8bf601fa20a549f
simoncos <l.z.simon@hotmail.com>
2018-11-07 12:03:05 +0800

    minor

--- selenium/test.py
+++ selenium/test.py
@@ -2,13 +2,16 @@ import time
 import platform
 from selenium import webdriver
 
+option = webdriver.ChromeOptions()
+option.add_argument('headless')
+
 # download and put chromedriver_to the path first
 if platform.system() == 'Windows':
-    driver = webdriver.Chrome('./chromedriver_win32.exe')
+    driver = webdriver.Chrome('./chromedriver_win32.exe', chrome_options=option)
 elif platform.system() == 'Linux':
-    driver = webdriver.Chrome('./chromedriver_linux')
+    driver = webdriver.Chrome('./chromedriver_linux', chrome_options=option)
 else:
-    driver = webdriver.Chrome('./chromedriver_mac')
+    driver = webdriver.Chrome('./chromedriver_mac', chrome_options=option)
 
 driver.get('https://movie.douban.com/top250')
 
@@ -22,6 +25,10 @@ while True:
               info.find_element_by_xpath('div[@class="info"]/div[@class="bd"]/p[@class="quote"]/span').text)
         # title = info.xpath('div[@class="pic"]/a/img/@alt').extract()
         # print(title)
-    next = driver.find_element_by_xpath('//span[@class="next"]/a')
+    try:
+        next = driver.find_element_by_xpath('//span[@class="next"]/a')
+    except:
+        print('Next page not found')
+        break
     time.sleep(5)
     next.click()
